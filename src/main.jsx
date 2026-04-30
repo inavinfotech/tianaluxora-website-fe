988689d3c1978dd210901fe1254deb7ece6fcb5e
DeveloperSwastik <developerswastiksharma@gmail.com>
2026-04-30 17:13:00 +0530

    feat: implement environment-based dynamic base URL routing for development and production modes

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { CartProvider } from "./contexts/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <ProductProvider>
           <CartProvider>
